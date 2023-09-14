8357383c6c8cd4067befbd9d3f3a94501fbe8c1f
nghiaomg <trungnghia.260716@gmail.com>
2023-09-14 14:09:13 +0700

    Update: root service is done

--- app/services/root.service.js
+++ app/services/root.service.js
@@ -1,6 +1,49 @@
 
 const db = require("../models/connectDb");
 
+exports.create = async (tableName, data, result) => {
+    try {
+        const query = `INSERT INTO ${tableName} SET ?`;
+
+        data.created_at = new Date();
+            
+        db.query(query, data, (err, dataRes) => {
+            if (err) {
+                console.log(err)
+                return result({ msg: 'Lỗi khi thêm dữ liệu' }, null);
+            }
+            result(null, dataRes);
+        });
+    } catch (error) {
+        result({ msg: error }, null);
+    }
+}
+
+exports.getById = async(tableName, id, fields, result) => {
+	try {
+        if( fields.trim() == '*') 
+            var selectedFields = '*';
+        else
+            var selectedFields = fields.map(field => `${tableName}.${field}`).join(', ');
+
+		const query = `SELECT ${selectedFields} FROM ${tableName} WHERE id = ?`;
+
+		db.query(query, [id], (err, dataRes) => {
+			if (err) {
+				console.log(err);
+				return result({
+					msg: 'Lỗi khi truy vấn dữ liệu'
+				}, null);
+			}
+			result(null, dataRes);
+		});
+	} catch (error) {
+		result({
+			msg: error
+		}, null);
+	}
+};
+
 exports.getall = async (tableName,page, resultsPerPage, result) => {
     try { 
         const offset = (page - 1) * resultsPerPage;
@@ -50,45 +93,93 @@ exports.getall = async (tableName,page, resultsPerPage, result) => {
     }
 };
 
-exports.isExists = (tableName,data) => {
-    data.forEach(element => {
-        // Lấy danh sách các key trong object element
-        const keys = Object.keys(element);
-      
-        // Lặp qua từng key và thực hiện các thao tác khác
-        keys.forEach(key => {
-          const exists = `SELECT COUNT(*) as count FROM ${tableName} WHERE ${key} = ?`;
-          console.log(key);
-          // Tiếp tục xử lý các thao tác khác với exists
-        });
-      });
-}
 
-exports.create = async (tableName,data, result) => {
-    try {
-        const query = `INSERT INTO ${tableName} SET ?`;
+exports.update = async(tableName, id, newData, result) => {
+	try {
+		const selectQuery = `SELECT * FROM ${tableName} WHERE id = ?`;
 
-        data.created_at = new Date();
+		db.query(selectQuery, [id], (selectErr, selectData) => {
+			if (selectErr) {
+				console.log(selectErr);
+				return result({
+					msg: 'Lỗi khi truy vấn dữ liệu cũ'
+				}, null);
+			}
 
-        const checkAliasQuery = `SELECT COUNT(*) as count FROM ${tableName} WHERE alias = ?`;
-        db.query(checkAliasQuery, [data.alias], (aliasErr, aliasResult) => {
-            if (aliasErr) {
-                return result({ msg: 'Lỗi kiểm tra alias' }, null);
-            }
-            
-            if (aliasResult[0].count > 0) {
-                return result({ msg: 'Alias đã tồn tại trong trước đó' }, null);
-            }
-            
-            db.query(query, data, (err, dataRes) => {
-                if (err) {
-                    console.log(err)
-                    return result({ msg: 'Lỗi khi thêm dữ liệu' }, null);
-                }
-                result(null, dataRes);
-            });
-        });
-    } catch (error) {
-        result({ msg: error }, null);
-    }
-}
+			if (selectData.length === 0) {
+				return result({
+					msg: 'Không tìm thấy dữ liệu để cập nhật'
+				}, null);
+			}
+
+			const oldData = selectData[0];
+
+			const updateQuery = `UPDATE ${tableName} SET ? WHERE id = ?`;
+
+			const updatedData = {...oldData, ...newData
+			};
+
+			db.query(updateQuery, [updatedData, id], (updateErr, updateDataRes) => {
+				if (updateErr) {
+					console.log(updateErr);
+					return result({
+						msg: 'Lỗi khi cập nhật dữ liệu'
+					}, null);
+				}
+				result(null, updateDataRes);
+			});
+		});
+	} catch (error) {
+		result({
+			msg: error
+		}, null);
+	}
+};
+
+exports.deleteById = async(tableName, id, result) => {
+	try {
+		const query = `DELETE FROM ${tableName} WHERE id = ?`;
+
+		db.query(query, [id], (err, dataRes) => {
+			if (err) {
+				console.log(err);
+				return result({
+					msg: 'Lỗi khi xóa dữ liệu'
+				}, null);
+			}
+			result(null, dataRes);
+		});
+	} catch (error) {
+		result({
+			msg: error
+		}, null);
+	}
+};
+
+exports.isExists = async(tableName, data) => {
+	try {
+		for (const element of data) {
+			const keys = Object.keys(element);
+
+			const promises = keys.map(async(key) => {
+				const query = 'SELECT COUNT(*) AS count FROM ?? WHERE ?? = ?';
+				const values = [tableName, key, element[key]];
+				const [rows] = await connection.promise().query(query, values);
+				return rows[0].count > 0;
+			});
+
+			const results = await Promise.all(promises);
+
+			if (results.includes(true)) {
+				return true;
+			}
+		}
+		return false;
+	} catch (error) {
+		console.error('Lỗi:', error);
+		return false;
+	} finally {
+		connection.end();
+		return false;
+	}
+};
